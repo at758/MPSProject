dd55abd8bdbf314b8d390b713668bdb760346205
Akshay Tata <at758@cornell.edu>
2016-02-06 14:34:18 -0500

    Latest copy

---
+++ CatListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CatListViewController.swift
+//  FitCat
+//
+//  Created by Akshay Tata on 2/6/16.
+//  Copyright © 2016 Akshay Tata. All rights reserved.
+//
+
+import UIKit
+
+class CatListViewController: UIViewController, UITableViewDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test Cell"
+        
+        return cell
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
